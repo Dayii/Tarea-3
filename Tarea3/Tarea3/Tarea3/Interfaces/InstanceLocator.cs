c512d4293e43f14304c663fa32e3b106b41a1110
dianateranaragandona@gmail.com <dianateranaragandona@gmail.com>
2019-06-26 18:45:47 -0400

    Commit3

---
+++ Tarea3/Tarea3/Tarea3/Interfaces/InstanceLocator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea3.Interfaces
+{
+    using ViewModels;
+
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
---
+++ Tarea3/Tarea3/Tarea3/Models/Currency.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea3.Models
+{
+    using Newtonsoft.Json;
+
+    public class Currency
+    {
+        [JsonProperty(PropertyName = "code")]
+        public string Code { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "symbol")]
+        public string Symbol { get; set; }
+    }
+}
